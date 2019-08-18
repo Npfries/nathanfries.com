ea4c73e0c8d765d5639b6de429132e4d40b0922c
Nathan Fries <28522519+Npfries@users.noreply.github.com>
2019-08-17 18:08:58 -0700

    renamded public folder to docs for pages

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.use(express.static('public'))
+app.use(express.static('docs'))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/index.html'))
+    res.sendFile(path.join(__dirname + '/docs/index.html'))
 })
 
 app.listen(3000)
