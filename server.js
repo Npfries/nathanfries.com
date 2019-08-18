91b0cd905f5f056304e0dd3204b97306e7003a5b
Nathan Fries <28522519+Npfries@users.noreply.github.com>
2019-08-17 18:05:49 -0700

    fixed links which were at /public folder

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.use('/public', express.static('public'))
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/public/index.html'))
