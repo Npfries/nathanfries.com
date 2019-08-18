f9f9c88cddf9f228280aa674d236737b10884e49
Nathan Fries <28522519+Npfries@users.noreply.github.com>
2019-08-17 18:14:20 -0700

    rerouted to /website for expres.static

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.use(express.static('docs'))
+app.use('/website', express.static('docs'))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/docs/index.html'))
