fd69ab16afd6f3f71f08e043a64160f276ab6f46
Nathan Fries <28522519+Npfries@users.noreply.github.com>
2019-08-18 21:26:46 -0700

    using workbox-build npm module to generate sw.js

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const workboxBuild = require('workbox-build')
 
 app.use(express.static('docs'))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/docs/index.html'))
 })
+
+// NOTE: This should be run *AFTER* all your assets are built
+const buildSW = () => {
+    // This will return a Promise
+    return workboxBuild.generateSW({
+      globDirectory: 'docs',
+      globPatterns: [
+        '**/*.{html,json,js,css,jpg,webp}',
+      ],
+      swDest: 'docs/sw.js',
+    });
+  };
+
+  buildSW()
 
 app.listen(3000)
