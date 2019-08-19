1d4d67290040e1987fdc34da97af8e3d0b2ce5cd
Nathan Fries <28522519+Npfries@users.noreply.github.com>
2019-08-18 22:02:26 -0700

    Revert "using workbox-build npm module to generate sw.js"
    This reverts commit fd69ab16afd6f3f71f08e043a64160f276ab6f46.

--- docs/sw.js
+++ docs/sw.js
@@ -1,85 +1,67 @@
-/**
- * Welcome to your Workbox-powered service worker!
- *
- * You'll need to register this file in your web app and you should
- * disable HTTP caching for this file too.
- * See https://goo.gl/nhQhGp
- *
- * The rest of the code is auto-generated. Please don't update this file
- * directly; instead, make changes to your Workbox build configuration
- * and re-run your build process.
- * See https://goo.gl/2aRDsh
- */
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
+workbox.routing.registerRoute(
+    /\.(?:js|html)$/,
+    new workbox.strategies.NetworkFirst()
+);
 
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
-self.__precacheManifest = [
-  {
-    "url": "assets/images/dashboard.jpg",
-    "revision": "43bdbec57098eecbd362376c9308f65e"
-  },
-  {
-    "url": "assets/images/dashboard.webp",
-    "revision": "a742de7e0ac7bd6a32bc2efd3f1d62aa"
-  },
-  {
-    "url": "assets/images/notifications.jpg",
-    "revision": "ff18511d265341f4a9f8c72c85a25ffb"
-  },
-  {
-    "url": "assets/images/notifications.webp",
-    "revision": "a3769376de1b387495a55fa85a7599ba"
-  },
-  {
-    "url": "assets/images/workorder-held.jpg",
-    "revision": "b9fd9402bcaae7894617f1830170e277"
-  },
-  {
-    "url": "assets/images/workorder-held.webp",
-    "revision": "9d95cc24ede6150ee9bb5d19f694d524"
-  },
-  {
-    "url": "assets/images/workorder.jpg",
-    "revision": "91d113f55e52cbafa4c2df6c9916be6a"
-  },
-  {
-    "url": "assets/images/workorder.webp",
-    "revision": "c35938ec004dbaf9ec70ad13121d3f44"
-  },
-  {
-    "url": "components/nf-modal.js",
-    "revision": "9f9bd84703afdf2fb34aabeb0ae0c83c"
-  },
-  {
-    "url": "index.html",
-    "revision": "14d6b24ad7f0ed5a7fb21b6f49d393df"
-  },
-  {
-    "url": "scripts/app.js",
-    "revision": "07a393ac8b8c348e57fb107fd591279d"
-  },
-  {
-    "url": "styles/dispiro.css",
-    "revision": "050ff56612d6becbb05eb76dbc3fbe8a"
-  },
-  {
-    "url": "styles/normalize.css",
-    "revision": "e1030567246d42c130844834dafa2249"
-  },
-  {
-    "url": "styles/style.css",
-    "revision": "082fb744e65e0a9eec2dac56add68a9e"
-  }
-].concat(self.__precacheManifest || []);
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+workbox.routing.registerRoute(
+    // Cache CSS files.
+    /\.css$/,
+    // Use cache but update in the background.
+    new workbox.strategies.StaleWhileRevalidate({
+      // Use a custom cache name.
+      cacheName: 'css-cache',
+    })
+  );
+  
+  workbox.routing.registerRoute(
+    // Cache image files.
+    /\.(?:png|jpg|jpeg|svg|gif)$/,
+    // Use the cache if it's available.
+    new workbox.strategies.CacheFirst({
+      // Use a custom cache name.
+      cacheName: 'image-cache',
+      plugins: [
+        new workbox.expiration.Plugin({
+          // Cache only 20 images.
+          maxEntries: 20,
+          // Cache for a maximum of a week.
+          maxAgeSeconds: 2 * 24 * 60 * 60,
+        })
+      ],
+    })
+  );
+
+  workbox.googleAnalytics.initialize();
+
+  // Cache the Google Fonts stylesheets with a stale-while-revalidate strategy.
+workbox.routing.registerRoute(
+  /^https:\/\/fonts\.googleapis\.com/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'google-fonts-stylesheets',
+  })
+);
+
+// Cache the underlying font files with a cache-first strategy for 1 year.
+workbox.routing.registerRoute(
+  /^https:\/\/fonts\.gstatic\.com/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'google-fonts-webfonts',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200],
+      }),
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 365,
+        maxEntries: 30,
+      }),
+    ],
+  })
+);
