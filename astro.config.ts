632be7ebbf701a34f08967124794ada1aaa4dfcc
Nathan Fries <npfries@gmail.com>
2023-06-18 18:21:26 -0400

    rm analytics

--- astro.config.ts
+++ astro.config.ts
@@ -4,7 +4,6 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
-import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
@@ -28,12 +27,6 @@ export default defineConfig({
 		}),
 		sitemap(),
 		prefetch(),
-		partytown({
-			// Adds dataLayer.push as a forwarding-event.
-			config: {
-				forward: ["dataLayer.push"],
-			},
-		}),
 	],
 	compressHTML: true,
 	vite: {
