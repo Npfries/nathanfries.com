a88dff8758a1d7af138b5e5842872b6f68148bc0
Nathan Fries <npfries@gmail.com>
2023-06-18 19:15:26 -0400

    add content to rss

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,6 +1,9 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import { siteConfig } from "@/site-config";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+const parser = new MarkdownIt();
 
 export const get = async () => {
 	const posts = await getCollection("post");
@@ -14,6 +17,7 @@ export const get = async () => {
 			description: post.data.description,
 			pubDate: post.data.publishDate,
 			link: `posts/${post.slug}`,
+			content: sanitizeHtml(parser.render(post.body)),
 		})),
 	});
 };
