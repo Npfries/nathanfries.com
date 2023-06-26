b2ab1f96d9603661e13ec39bf0e0673b3c4831a8
Nathan Fries <npfries@gmail.com>
2023-06-26 01:19:56 -0400

    add description to post previews on index page

--- src/site.config.ts
+++ src/site.config.ts
@@ -20,15 +20,15 @@ export const siteConfig: SiteConfig = {
 	// Meta property used as a default description meta property
 	description: "An opinionated platform engineer",
 	// HTML lang property, found in src/layouts/Base.astro L:18
-	lang: "en-GB",
+	lang: "en-US",
 	// Meta property, found in src/components/BaseHead.astro L:42
-	ogLocale: "en_GB",
+	ogLocale: "en_US",
 	// Sets the meta data theme-color, found in src/components/BaseHead.astro L:34. Toggling the dark mode will update the meta content with either light/dark color, implementation in src/layouts/Base.astro L:41.
 	themeColorLight: "#fafafa",
 	themeColorDark: "#1d1f21",
 	// Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
 	date: {
-		locale: "en-GB",
+		locale: "en-US",
 		options: {
 			day: "numeric",
 			month: "short",
